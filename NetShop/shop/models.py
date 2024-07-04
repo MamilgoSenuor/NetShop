70e3c35a6159d05fb5a4302becdce4d8895a722f
RealCupcake <goirel009@gmail.com>
2024-07-04 22:59:46 +0300

    Исправленны баги и добавлены характеристики

--- NetShop/shop/models.py
+++ NetShop/shop/models.py
@@ -25,6 +25,3 @@ class Review(models.Model):
     author = models.CharField(max_length=256)
     rating = models.IntegerField()
     text = models.TextField()
-
-    def __str__(self):
-        return f'{self.author}: {self.text[:100]}'
--- NetShop/shop/views.py
+++ NetShop/shop/views.py
@@ -1,26 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
 from .models import Product
-
 # Create your views here.
-
 def home(request):
-    search = request.GET.get('search')
-
-    if search:
-        products = Product.objects.filter(name__contains=search).all()
-    else:
-        products = Product.objects.all()
-
+    products = Product.objects.all()
     return render(request, "index.html", {
-        'products': products,
-        'products_found': len(products) > 0,
-        'search': search if search else '',
+        'products': products
     })
 
 def view_product(request, id):
     product = Product.objects.filter(id=id).first()
+    reviews = product.review_set.all()
+
     return render(request, 'product.html', {
-        'product': product
+        'product': product,
+        'reviews': reviews
     })
